7640b4dd7669ef1395adab1e7d0010f8c80f1ad5
FRANCO1357 <fmelani13@gmail.com>
2022-06-27 22:37:11 +0200

    edit dadi

--- dadi/script.js
+++ dadi/script.js
@@ -5,16 +5,19 @@
 
 
 
-const playerNumber = Math.floor(Math.random() * 5) +1;
+const playerNumber = Math.floor(Math.random() * 6) +1;
 console.log('user number:' + playerNumber);
 
-const pcNumber = Math.floor(Math.random() * 5) +1;
+const pcNumber = Math.floor(Math.random() * 6) +1;
 console.log('computer number:' + pcNumber);
 
+let message;
 if (playerNumber > pcNumber){
-    console.log('Ha vinto il giocatore');
+    message = 'Ha vinto il giocatore';
 } else if (playerNumber < pcNumber){
-    console.log('Ha vinto il computer');
+    message = 'Ha vinto il computer';
 } else {
-    console.log('I due numeri sono uguali');
+    message = 'I due numeri sono uguali';
 }
+
+console.log(message);
